43fc920c4f23590e456a180d1ffee66628f8894e
Igor <igor.olshanskyi@gmail.com>
2026-01-22 16:08:08 +0200

    test_2

--- src/server/app.js
+++ src/server/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../client')));
-app.use('/shared', express.static(path.join(__dirname, '../../../shared')));
+app.use('/shared', express.static(path.join(__dirname, '../../shared')));
 app.get('/api/rooms', roomsController.getRooms);
 app.post('/api/rooms', roomsController.createRoom);
 
